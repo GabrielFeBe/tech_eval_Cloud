98b189f89120e4bd75539b4e012323cbd8b378d9
Gabriel Fernandes <hu3master.zord@hotmail.com>
2024-05-13 13:53:14 -0300

    terminar a parte de criação no FE e no BE

---
+++ backend/src/controller/filmes.controller.js
@@ -0,0 +1,43 @@
+const service = require('../service/filmeService')
+
+const buscaFilme = async (req, res) => {
+  const { id } = req.params;
+  const filme = await service.buscaFilme(id);
+  if(!filme) return res.status(404).json({ message: 'Filme não encontrado' });
+  res.status(200).json(filme);
+}
+
+const buscaFilmes = async (req, res) => {
+  const filmes = await service.buscaFilmes();
+  res.status(200).json(filmes);
+}
+
+const criaFilme = async (req, res) => {
+  const { titulo, anoDeLancamento, estaDisponivel } = req.body;
+  if(!titulo || !anoDeLancamento || !estaDisponivel) return res.status(400).json({ message: 'Dados inválidos' });
+  const filme = await service.criaOuAtualizaFilme({ titulo, anoDeLancamento, estaDisponivel });
+  res.status(201).json(filme);
+}
+
+const atualizaFilme = async (req, res) => {
+  const { id } = req.params;
+  const { titulo, anoDeLancamento, estaDisponivel } = req.body;
+  if(!titulo || !anoDeLancamento || !estaDisponivel) return res.status(400).json({ message: 'Dados inválidos' });
+  const filme = await service.criaOuAtualizaFilme({ id, titulo, anoDeLancamento, estaDisponivel });
+  res.status(200).json(filme);
+}
+
+
+const deletaFilme = async (req, res) => {
+  const { id } = req.params;
+  await service.deletaFilme(id);
+  res.status(204).end();
+}
+
+module.exports = {
+  buscaFilme,
+  buscaFilmes,
+  criaFilme,
+  atualizaFilme,
+  deletaFilme
+}
--- frontend/src/app/criar/filmes/page.js
+++ frontend/src/app/criar/filmes/page.js
@@ -1,5 +1,6 @@
 'use client'
 import Input from "@/app/components/Input";
+import endpoint from "@/utils/endpoint";
 import { useState } from "react";
 
 export default function Page() {
@@ -9,10 +10,31 @@ export default function Page() {
     disponivel: false,
   });
   
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(endpoint.concat('/filmes'), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(filme),
+      });
+      
+      if (!response.ok) {
+        const jsonMsg = await response.json();
+        throw new Error('Não foi possível criar o filme por'.concat(' - ').concat(jsonMsg.message));
+      }
+      const data = await response.json();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Criar Filmes</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <Input label="Título" name="titulo" type="text" state={filme} setState={setFilme} value={filme.titulo} className="rounded-md pl-[5px]"/>
         <Input label="Ano" name="ano" type="number" state={filme} setState={setFilme} value={filme.ano} className="rounded-md pl-[5px]"/>
         <Input label="Está disponível?" name="disponivel" type="checkbox" state={filme} setState={setFilme} value={filme.disponivel}/>
